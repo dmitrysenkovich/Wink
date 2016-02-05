acbfded73b78f791f2d110e8f73d6223982edaf5
Dmitry Senkovich <dmitrysenkovich@gmail.com>
2016-02-05 23:05:50 +0300

    before drag and drop

---
+++ js/effects-custom.js
@@ -0,0 +1,47 @@
+/* -----------------------------
+Pre Loader
+----------------------------- */
+$(window).load(function() {
+	'use strict';
+	$('.loading-icon').delay(500).fadeOut();
+	$('#preloader').delay(800).fadeOut('slow');
+});
+
+/* -----------------------------
+Scroll into viewPort Animation
+----------------------------- */
+$(document).ready(function() {
+	'use strict';
+	$('.animated').appear(function() {
+		var element = $(this),
+			animation = element.data('animation'),
+			animationDelay = element.data('animation-delay');
+			if ( animationDelay ) {
+
+				setTimeout(function(){
+					element.addClass( animation + " visible");
+				}, animationDelay);
+
+			} else {
+				element.addClass( animation + " visible");
+			}
+	});
+});
+
+
+/* -----------------------------
+NiceScroll
+----------------------------- */
+$(document).ready(function() {
+	'use strict';
+    $("html").niceScroll({
+		cursorcolor: '#95A5A6',
+		cursoropacitymin: '1',
+		cursorborder: '0px',
+		cursorborderradius: '0px',
+		cursorwidth: '5px',
+		cursorminheight: 60,
+		horizrailenabled: false,
+		zindex: 1090
+	});
+  });
